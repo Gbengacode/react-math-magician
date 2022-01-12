1a9a6ceb409ced958672918071c11c278805f747
benjp009 <patin.benjamin@gmail.com>
2022-01-12 13:14:46 +0100

    Add Snapshots and correct operate.test.js

--- src/__test__/operate.test.js
+++ src/__test__/operate.test.js
@@ -1,20 +1,35 @@
-import Big from 'big.js';
-import obj from '../__mock__/operate';
-
-describe('Operate',(one, two) => {
-  test('the test is to check that adding works', () => {
-    expect(one + two).toEqual(one + two);
+import operate from '../logic/operate';
+describe('Operate', () => {
+  const num1 = 4;
+  const num2 = 2;
+  const addition = operate(num1, num2, '+');
+  test(' The test check for  Addition of two number', () => {
+    expect(addition).toBe('6');
+    expect(addition).toMatchSnapshot();
   });
-
-  test('the test is to check that substract works', () => {
-    expect(one - two).toEqual(one - two);
+  const subtraction = operate(num1, num2, '-');
+  test('The test check for subtraction of two number', () => {
+    expect(subtraction).toBe('2');
+    expect(subtraction).toMatchSnapshot();
   });
-
-  test('the test is to check that multiply works', () => {
-    expect(one * two).toEqual(one * two);
+  const division = operate(num1, num2, '÷');
+  test(' The test check for division of two number', () => {
+    expect(division).toBe('2');
+    expect(division).toMatchSnapshot();
   });
-
-  test('the test is to check that divide works', () => {
-    expect(one / two).toEqual(one / two);
+  const multiplication = operate(num1, num2, 'x');
+  test('The test check for multiplication of two number', () => {
+    expect(multiplication).toBe('8');
+    expect(multiplication).toMatchSnapshot();
+  });
+  const noDivisibility = operate(num1, '0', '÷');
+  test('The  test that check for no divisibility by denominator zero', () => {
+    expect(noDivisibility).toBe("Can't divide by 0.");
+    expect(noDivisibility).toMatchSnapshot();
+  });
+  const modulus = operate(num1, num2, '%');
+  test('The  test that checks for the modulus of a number', () => {
+    expect(modulus).toBe('0');
+    expect(modulus).toMatchSnapshot();
   });
 });
